26de58916ea1b9d432c648f7873bf9c742a4b528
吉川直人 <yoshikawanaoto@yoshikakonpyuta.AirPort>
2024-03-24 18:17:25 +0900

    gsap_add

--- assets/gsap.js
+++ assets/gsap.js
@@ -1,5 +1,21 @@
 //console.log('test');
 document.addEventListener('DOMContentLoaded', function() {
-    gsap.fromTo(".target", { x: 100 }, { x: 200, duration: 1 });
+
+
+
+
+    gsap.to('.target', { // .box => アニメーションさせる要素
+        x: 400, // アニメーションの内容
+        scrollTrigger: {
+          trigger: '.target', // トリガーとなる要素（アニメーションの開始、終了の位置決定に使用する）
+          start: 'center center', // アニメーションの開始位置の指定
+          toggleActions: 'play none none reverse', // スクロールアップでアニメーションを逆再生
+        }
+
+
+
+
+
+      });
 });
 
